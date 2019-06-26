78202e73c634af825221a87b3f6745717f60048d
Ing. Dexus José Pérez <joser.perezmiranda@gmail.com>
2019-06-26 17:51:24 -0500

    Agregado el Menu de opciones y la opcion de proteger.

--- src/BarraMenu/barMenu.java
+++ src/BarraMenu/barMenu.java
@@ -24,15 +24,27 @@ public class barMenu extends JMenuBar{
     // ******* Declaración de items *******
     
     private JMenuItem It_Salir,prueba;
-    private JMenu M_Archivo;
+    private JMenuItem It_Seguridad;
     private JMenuItem It_hojaCosto,It_NMDB;
     
-    // ******* Nombres de los items ******
+    private JMenu M_Archivo,M_Opciones;
+    
+    
+    // ******* Variables de la barra de menu ******
     
     private String MnArchivo = "Archivo";
+    private String MnOpciones= "Opciones";
+    
+    //******** Variables de los submenus ***********
+    
+    //-------- Submenu Archivo --------------
     private String itmSalir = "Salir";
     private String itmHojaregistro = "Registro costo $";
     private String itmNMDB = "Nuevo MDB";
+    
+    
+    //--------- Submenu Opciones ---------
+    private String itmCrearUsuario = "Proteger";
     
     
     public void barra(){
@@ -40,10 +52,14 @@ public class barMenu extends JMenuBar{
         setBackground(principal.BarraM2);
         
         M_Archivo = new JMenu();
+        M_Opciones = new JMenu();
+        
         It_Salir = new JMenuItem();
         It_hojaCosto = new JMenuItem();
         It_NMDB= new JMenuItem();
         prueba = new JMenuItem();
+        
+        It_Seguridad = new JMenuItem();
         
         M_Archivo.setText(MnArchivo);
         M_Archivo.setFont(principal.ArialB14);
@@ -81,13 +97,25 @@ public class barMenu extends JMenuBar{
             
         });
         
+        
         M_Archivo.add(It_hojaCosto);
         M_Archivo.add(It_NMDB);
         M_Archivo.add(It_Salir);
         M_Archivo.add(prueba);
         
         
+        M_Opciones.setText(MnOpciones);
+        M_Opciones.setFont(principal.ArialB14);
+        
+        It_Seguridad.setText(itmCrearUsuario);
+        It_Seguridad.setFont(principal.ArialB12);
+        
+        
+        M_Opciones.add(It_Seguridad);
+        
+        
         add(M_Archivo);
+        add(M_Opciones);
         
     }
     
