b867f8af15e7327cea9506ad1186100cceebc639
Ing. Dexus José Pérez <joser.perezmiranda@gmail.com>
2019-07-24 17:57:34 -0500

    Adhesion del frame de generacion de codificacion y el diseño.

--- src/pruebas/testCodeGen.java
+++ src/pruebas/testCodeGen.java
@@ -26,12 +26,17 @@ package pruebas;
 import MenuPrincipal.principal;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.PopupMenu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import static javax.swing.SwingConstants.CENTER;
 import javax.swing.border.BevelBorder;
 
 /**
@@ -40,6 +45,8 @@ import javax.swing.border.BevelBorder;
  */
 public class testCodeGen extends JFrame {
 
+    // *¨**** Designacion de,los campos de las etiquetas******
+    
     private JLabel l_texto;
     private JButton bt_setTes;
     private JLabel l_nombre;
@@ -49,6 +56,22 @@ public class testCodeGen extends JFrame {
     private JLabel l_precio;
     private JLabel l_folFact;
     private JLabel l_fech;
+    
+    // ***** Designacion de los campos de texto
+    
+    private JTextField tf_nombre;
+    private JTextField tf_descripcion;
+    private JTextField tf_marca;
+    private JTextField tf_codigoP;
+    private JTextField tf_precio;
+    private JTextField tf_folFact;
+    private JTextField tf_fech;
+    
+    //**** boton para generar el codigo ****
+    private JButton bt_Generar;
+    
+    private JTextField tf_codigoGenerado;
+    private JLabel l_textGen;
     
     public void generador(){
         
@@ -66,51 +89,108 @@ public class testCodeGen extends JFrame {
         l_folFact = new JLabel("Folio factura");
         l_fech = new JLabel("Fecha");
         
-        l_nombre.setFont(MenuPrincipal.principal.ArialB14);
-        l_nombre.setBounds(165, 100, 60, 30);
-        l_nombre.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        tf_nombre = new JTextField();
+        tf_descripcion = new JTextField();
+        tf_marca = new JTextField();
+        tf_codigoP = new JTextField();
+        tf_precio = new JTextField();
+        tf_folFact = new JTextField();
+        tf_fech = new JTextField();
         
-        l_descripcion.setFont(MenuPrincipal.principal.ArialB14);
-        l_descripcion.setBounds(375, 100, 90, 30);
-        l_descripcion.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        bt_Generar = new JButton("Generar");
         
-        l_marca.setFont(MenuPrincipal.principal.ArialB14);
-        l_marca.setBounds(615, 100, 50, 30);
-        l_marca.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        tf_codigoGenerado = new JTextField();
+        
+        l_textGen = new JLabel();
+        
+        l_nombre.setFont(MenuPrincipal.principal.ArialB12);
+        l_nombre.setHorizontalAlignment(CENTER);
+        l_nombre.setBounds(114, 100, 114, 30);
+        //l_nombre.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        
+        tf_nombre.setBounds(57, 140, 228, 30);
+        tf_nombre.setHorizontalAlignment(CENTER);
+        tf_nombre.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.darkGray));
+        
+        l_descripcion.setFont(MenuPrincipal.principal.ArialB12);
+        l_descripcion.setHorizontalAlignment(CENTER);
+        l_descripcion.setBounds(342, 100, 114, 30);
+        //l_descripcion.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        
+        tf_descripcion.setBounds(285, 140, 228, 30);
+        tf_descripcion.setHorizontalAlignment(CENTER);
+        tf_descripcion.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.darkGray));
+        
+        l_marca.setFont(MenuPrincipal.principal.ArialB12);
+        l_marca.setHorizontalAlignment(CENTER);
+        l_marca.setBounds(570, 100, 114, 30);
+        //l_marca.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        
+        tf_marca.setBounds(513, 140, 228, 30);
+        tf_marca.setHorizontalAlignment(CENTER);
+        tf_marca.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.darkGray));
         
         
         
-        l_codigoP.setFont(MenuPrincipal.principal.ArialB14);
-        l_codigoP.setBounds(100, 200, 60, 30);
-        l_codigoP.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        l_codigoP.setFont(MenuPrincipal.principal.ArialB12);
+        l_codigoP.setHorizontalAlignment(CENTER);
+        l_codigoP.setBounds(88, 200, 88, 30);
+        //l_codigoP.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
         
-        l_precio.setFont(MenuPrincipal.principal.ArialB14);
-        l_precio.setBounds(270, 200, 50, 30);
-        l_precio.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        tf_codigoP.setBounds(44, 240, 176, 30);
+        tf_codigoP.setHorizontalAlignment(CENTER);
+        tf_codigoP.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.darkGray));
         
-        l_folFact.setFont(MenuPrincipal.principal.ArialB14);
-        l_folFact.setBounds(420, 200, 90, 30);
-        l_folFact.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        l_precio.setFont(MenuPrincipal.principal.ArialB12);
+        l_precio.setHorizontalAlignment(CENTER);
+        l_precio.setBounds(264, 200, 88, 30);
+        //l_precio.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
         
-        l_fech.setFont(MenuPrincipal.principal.ArialB14);
-        l_fech.setBounds(650, 200, 50, 30);
-        l_fech.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        tf_precio.setBounds(220, 240, 176, 30);
+        tf_precio.setHorizontalAlignment(CENTER);
+        tf_precio.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.darkGray));
+        
+        l_folFact.setFont(MenuPrincipal.principal.ArialB12);
+        l_folFact.setHorizontalAlignment(CENTER);
+        l_folFact.setBounds(440, 200, 88, 30);
+        //l_folFact.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        
+        tf_folFact.setBounds(396, 240, 176, 30);
+        tf_folFact.setHorizontalAlignment(CENTER);
+        tf_folFact.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.darkGray));
+        
+        l_fech.setFont(MenuPrincipal.principal.ArialB12);
+        l_fech.setHorizontalAlignment(CENTER);
+        l_fech.setBounds(616, 200, 88, 30);
+        //l_fech.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
+        
+        tf_fech.setBounds(572, 240, 176, 30);
+        tf_fech.setHorizontalAlignment(CENTER);
+        tf_fech.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.darkGray));
+        
+        bt_Generar.setFont(MenuPrincipal.principal.ArialB14);
+        bt_Generar.setBounds(333,320,133,30);
+        bt_Generar.addActionListener(new ActionListener(){
+            public void  actionPerformed(ActionEvent evt){
+                generarCodigo();
+            }
+
+            
+        });
+        
+        l_textGen.setText("Codigo:");
+        l_textGen.setFont(MenuPrincipal.principal.ArialB14);
+        l_textGen.setHorizontalAlignment(CENTER);
+        l_textGen.setBorder(new BevelBorder(BevelBorder.LOWERED,Color.lightGray,Color.darkGray));
+        l_textGen.setBounds(88, 380, 88, 30);
         
         
+        tf_codigoGenerado.setFont(MenuPrincipal.principal.ArialP14);
+        tf_codigoGenerado.setBorder(new BevelBorder(BevelBorder.LOWERED,Color.lightGray,Color.darkGray));
+        tf_codigoGenerado.setHorizontalAlignment(CENTER);
+        tf_codigoGenerado.setBounds(176,380,440,30);
         
-//        l_texto = new JLabel();
-//        l_texto.setBorder(new BevelBorder(BevelBorder.RAISED, Color.lightGray, Color.darkGray));
-//        l_texto.setText("hello");
-//        l_texto.setFont(MenuPrincipal.principal.ArialB14);
-//        //l_texto.setPreferredSize(new Dimension(80,30));
-//        l_texto.setBounds(0,0,50,30);
-//        
-//        bt_setTes = new JButton();
-//        //bt_setTes.setBorder(new BevelBorder(HEIGHT, Color.lightGray, Color.yellow));
-//        bt_setTes.setText("Gua");
-//        //bt_setTes.setPreferredSize(new Dimension(100,50));
-//        bt_setTes.setBounds(200,200,100,50);
-//        
+
         this.setTitle("Generador de codigo");
         this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.addWindowListener(new WindowAdapter(){
@@ -130,9 +210,22 @@ public class testCodeGen extends JFrame {
         this.add(l_folFact);
         this.add(l_precio);
         
+        this.add(tf_nombre);
+        this.add(tf_marca);
+        this.add(tf_descripcion);
+        this.add(tf_folFact);
+        this.add(tf_precio);
+        this.add(tf_fech);
+        this.add(tf_codigoP);
+        
+        this.add(bt_Generar);
+        
+        this.add(tf_codigoGenerado);
+        
+        this.add(l_textGen);
+        
         this.setLayout(null);
-        //this.add(bt_setTes);
-        //this.add(l_texto);
+        
         this.pack();
         this.setLocationRelativeTo(null);
         this.setResizable(false);
@@ -140,6 +233,28 @@ public class testCodeGen extends JFrame {
         
     }
     
+    private void generarCodigo() {
     
+        //tf_codigoGenerado.setText("Hola");
+        char[] articulo = tf_nombre.getText().toCharArray();
+        char[] marca = tf_marca.getText().toCharArray();
+        char[] descripcion = tf_descripcion.getText().toCharArray();
+        
+        String codigo = tf_codigoP.getText();
+        String costo = tf_precio.getText();
+        String folio = tf_folFact.getText();
+        String fecha = tf_fech.getText();
+        
+        String result;
+        
+        for(int x = 0; x <= (articulo.length -1) ;x++){
+            System.out.println(articulo[x]);
+        }
+        
+        result = null;
+        
+        tf_codigoGenerado.setText(result);
+    
+    }
     
 }
